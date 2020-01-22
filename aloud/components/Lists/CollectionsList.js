a5208d253872314a7f13e7a326c1034cecd5d47a
addupe <autumndupin@gmail.com>
2020-01-22 17:19:58 -0600

    CollectionsList component

---
+++ aloud/components/Lists/CollectionsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+
+import Colors from '../../constants/Colors';
+import { ListItem, Text } from 'react-native-elements';
+
+export default function CollectionsList(props) {
+  return (
+   <Text>collections list</Text>
+  );
+}

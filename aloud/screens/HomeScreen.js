c9051e27167b885ad3707ccd65f389b51b1c6a55
addupe <autumndupin@gmail.com>
2020-01-22 17:19:36 -0600

    sample sound render homepage

--- aloud/screens/HomeScreen.js
+++ aloud/screens/HomeScreen.js
@@ -1,5 +1,7 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
+import CollectionsList from '../components/Lists/CollectionsList';
+import RecordingsList from '../components/Lists/RecordingsList';
 import {
   Image,
   Platform,
@@ -28,43 +30,10 @@ export default function HomeScreen() {
             style={styles.welcomeImage}
           />
         </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
+        <View>
+        <RecordingsList /> 
         </View>
       </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
     </View>
   );
 }

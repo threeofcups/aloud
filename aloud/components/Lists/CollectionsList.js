571e8d9123d7d043937c72da67331f5e83159361
addupe <autumndupin@gmail.com>
2020-01-22 20:10:40 -0600

    rendering collection list

--- aloud/components/Lists/CollectionsList.js
+++ aloud/components/Lists/CollectionsList.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CollectionsListItem from '../ListItems/CollectionsListItem';
 import { Ionicons } from '@expo/vector-icons';
+import collData from '../../src/sampleCollData';
 
 import Colors from '../../constants/Colors';
-import { ListItem, Text } from 'react-native-elements';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function CollectionsList(props) {
+  const [collections, setCollections] = useState(collData);
+
   return (
-   <Text>collections list</Text>
+    <View>
+      <ScrollView>
+        <Text>{'    '}collections</Text>
+        <View>
+          {
+            collData.map((collection, i) => {
+              return <CollectionsListItem key={i} collection={collection} />
+            })
+          }
+        </View>
+      </ScrollView>
+    </View>
   );
 }

ce6c658f8ce0fde3858d013478f00c53a3a695bf
addupe <autumndupin@gmail.com>
2020-01-22 20:10:28 -0600

    remove excess code library view

--- aloud/screens/LibraryScreen.js
+++ aloud/screens/LibraryScreen.js
@@ -7,13 +7,7 @@ export default function LibraryScreen() {
    const [value, onChangeText] = React.useState('Search term')
   return (
 <View>
-  {/* // Todo <Icon> magnifying glass</Icon> */}
-  <TextInput
-  style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-  onChangeText={text => onChangeText(text)}
-  value={value}/>
-  <RecordingsListItem/>
-
+  <Text>library view</Text>
 </View>
   );
 }

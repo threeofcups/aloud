c5ef2e6db2e21ebad1cbf62f049e8fa6614aac2b
addupe <autumndupin@gmail.com>
2020-01-22 18:54:47 -0600

    rendering sample recording lists

--- aloud/components/Lists/RecordingsList.js
+++ aloud/components/Lists/RecordingsList.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import RecordingsListItem from '../ListItems/RecordingsListItem'
 import { Ionicons } from '@expo/vector-icons';
+import recData from '../../src/sampleRecData';
 
 import Colors from '../../constants/Colors';
 import {
@@ -8,16 +9,21 @@ import {
   Text,
   View,
 } from 'react-native';
+import { ListItem } from 'react-native-elements';
 
 export default function RecordingsList(props) {
+  const [recordings, setRecordings] = useState(recData);
+
   return (
     <View>
     <Text>recordings list</Text>
-      <RecordingsListItem />
-      <RecordingsListItem />
-      <RecordingsListItem />
-      <RecordingsListItem />
-      <RecordingsListItem />
+    <View>
+      {
+        recData.map((recording, i) => {
+         return <RecordingsListItem recording={recording}/>
+        })
+      }
+    </View>
     </View>
   );
 }

ca64f17ef84d2cf991fc92346cb4a550442589de
addupe <autumndupin@gmail.com>
2020-01-22 20:10:58 -0600

    rendering collection list items

--- aloud/components/ListItems/CollectionsListItem.js
+++ aloud/components/ListItems/CollectionsListItem.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
 
-import Colors from '../constants/Colors';
+import Colors from '../../constants/Colors';
+import { View, StyleSheet } from 'react-native';
+import { ListItem } from 'react-native-elements';
 
-export default function CollectionsListItem(props) {
+export default function CollectionsListItem({collection}) {
   return (
-    <Ionicons
-      name={props.name}
-      size={26}
-      style={{ marginBottom: -3 }}
-      color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+    <ListItem
+      leftAvatar={{
+        source: { uri: "https://worldofwonder.net/wp-content/uploads/2017/03/3-27-frank-ohara.jpg" } }}
+      title={collection.title}
+      subtitle={collection.user}
+      rightIcon={{ name: 'more-horiz' }}
     />
   );
 }

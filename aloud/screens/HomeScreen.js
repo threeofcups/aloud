cce15ffbb5526e81b17ee70dc8f38be014884153
addupe <autumndupin@gmail.com>
2020-01-22 19:49:48 -0600

    comment out image on home screen

--- aloud/screens/HomeScreen.js
+++ aloud/screens/HomeScreen.js
@@ -21,14 +21,14 @@ export default function HomeScreen() {
         style={styles.container}
         contentContainerStyle={styles.contentContainer}>
         <View style={styles.welcomeContainer}>
-          <Image
+          {/* <Image
             source={
               __DEV__
                 ? require('../assets/images/robot-dev.png')
                 : require('../assets/images/robot-prod.png')
             }
             style={styles.welcomeImage}
-          />
+          /> */}
         </View>
         <View>
         <RecordingsList /> 

555fb2b1c32d97ed9753f83e79e57bdc53f367aa
addupe <autumndupin@gmail.com>
2020-01-22 19:50:47 -0600

    update recording items w sample data

--- aloud/components/ListItems/RecordingsListItem.js
+++ aloud/components/ListItems/RecordingsListItem.js
@@ -1,23 +1,35 @@
 // avatar, sound name, artist name, length, more
 import React from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import { Platform } from 'react-native';
 
 import Colors from '../../constants/Colors';
-import { ListItem, View } from 'react-native-elements';
+import { View, StyleSheet } from 'react-native';
+import { ListItem } from 'react-native-elements';
 
 export default function RecordingsListItem({recording}) {
-  const config = Platform.select({
-    web: { headerMode: 'false' },
-    default: {},
-  });
 
   return (
-    <ListItem
-    size="small"
-    title={recording.title}
-    subtitle={recording.id_user}
-    chevron
-  />
+    <View style={styles.container}>
+      <ListItem
+        leftIcon={{ name: "play-circle-filled"}}
+        title={recording.title}
+        subtitle={recording.user}
+        rightIcon={{ name: 'more-horiz'}}
+      />
+    </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eac3cd',
+  },
+  playIcon: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  }
+});

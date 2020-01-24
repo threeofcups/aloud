298770bb637883a5d4719ddcc58a5fe2c6fc1e25
Jill Poole <jillianntish@gmail.com>
2020-01-24 11:56:45 -0600

    start axios call to cloudinary to save sounds

--- aloud/screens/RecordScreen.js
+++ aloud/screens/RecordScreen.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { ExpoConfigView } from '@expo/samples';
 import { Ionicons } from '@expo/vector-icons';
 import {
@@ -13,14 +15,32 @@ import {
 
 export default function RecordScreen() {
 
+  const [recFlash, recFlasher] = useState(null);
+
+  const recCall = function(){
+    useEffect(() => {
+    return axios.get('https://aloud-server.appspot.com/recording')
+    .then(response => {
+      recFlasher(response.data)
+    })
+    .catch(err => console.log('there was an axios err', err))
+  });
+  }
+
   return (
     <View allignItems={'center'}>
 
       <Ionicons name={'md-mic'}
       size={300}
-      onPress={()=>{console.log('dot')}}
+      onPress={()=>{
+        return axios.get('https://aloud-server.appspot.com/recording')
+    .then(response => {
+      console.log(response.data)
+      recFlasher(response.data)
+    })
+    .catch(err => console.log('there was an axios err', err))
+      }}
       />
-      
     </View>
   );
 }

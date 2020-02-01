eaa66e0dc2b94dad865d40281465ce104dc13661
Jill Poole <jillianntish@gmail.com>
2020-02-01 13:48:25 -0600

    clean up

--- aloud/screens/RecordScreen.js
+++ aloud/screens/RecordScreen.js
@@ -355,7 +355,6 @@ export default class RecordScreen extends React.Component {
     .catch(err => console.log('media library save asset err', err))
     }
 
-    
     async uploadRecFromPhone(){
       DocumentPicker.getDocumentAsync({
         type: '*/*',
@@ -367,24 +366,18 @@ export default class RecordScreen extends React.Component {
         console.log(succ, `path: ${succ.uri}, type: ${succ.type}, name: ${succ.id}, size: ${succ.size}`)
         //https://www.iana.org/assignments/media-types/media-types.xhtml#audio - MIME audio types
         var Base64 = {
-
           // private property
           _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-          
           // public method for encoding
           encode : function (input) {
               var output = "";
               var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
               var i = 0;
-          
               input = Base64._utf8_encode(input);
-          
               while (i < input.length) {
-          
                   chr1 = input.charCodeAt(i++);
                   chr2 = input.charCodeAt(i++);
                   chr3 = input.charCodeAt(i++);
-          
                   enc1 = chr1 >> 2;
                   enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
                   enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);

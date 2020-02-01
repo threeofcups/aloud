8bc059d7b2cdc2eec1521cd8162e1937337f3d0f
addupe <autumndupin@gmail.com>
2020-01-31 19:58:40 -0600

    set up to render recordings in modal on collection press

--- aloud/components/ListItems/RecentListItem.js
+++ aloud/components/ListItems/RecentListItem.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import { Card } from 'react-native-elements';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import { Card, Text, Button } from 'react-native-elements';
+import { Modal } from 'react-native';
+import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import CollectionsScreen from '../../screens/CollectionScreen'
 
 export default function RecentListItem ({ collection }) {
+  const [modalVisible, setModalVisibilty] = useState(false);
+
+  const onPressHandle = () => {
+    console.log('COLLECTION', collection);
+    setModalVisibilty(!modalVisible);
+  };
+
+  if (modalVisible) {
+    return (
+      <Modal
+        animationType="fade"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisibilty(!modalVisible)
+        }}>
+      <Text style={{ marginTop: 54, textAlign: "center" }}>{collection.title}</Text>
+      <Text style={{ margin: 22, textAlign: "center" }}>render all {collection.title} recordings here</Text>
+      <Button
+          title="x"
+          type="clear"
+          onPress={() => {
+            setModalVisibilty(!modalVisible);
+          }}
+      />
+      </Modal>
+    )
+  } 
 
     return (
+      <TouchableWithoutFeedback onPress={() => {onPressHandle()}}>
         <Card
           containerStyle={{ padding: 0, width: 150, height: 150, borderWidth: 0, marginBottom: 15 }}
           image={{ uri: collection.url_image }}
           // title={collection.title} 
           featuredSubtitle={collection.title}
         />
+      </TouchableWithoutFeedback>
     )
 
 }

94867a46929d0a53d55355346a11ab4e31b2670f
addupe <autumndupin@gmail.com>
2020-02-01 14:18:28 -0600

    edit collections view for collections on press

--- aloud/components/ListItems/CollectionsListItem.js
+++ aloud/components/ListItems/CollectionsListItem.js
@@ -1,36 +1,75 @@
-import React, {useState} from 'react';
-import { Ionicons } from '@expo/vector-icons';
-
-import Colors from '../../constants/Colors';
-import { View, StyleSheet, Image, Text } from 'react-native';
-import { ListItem, Card } from 'react-native-elements';
-import { TouchableHighlight, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Card, Text, Button, Image } from 'react-native-elements';
+import { Modal, View } from 'react-native';
+import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import RecordingsList from '../Lists/RecordingsList';
 import CollectionsScreen from '../../screens/CollectionScreen'
 
-export default function CollectionsListItem({ collection, listOrCollection, toggleListOrCollection }) {
-  // const [listOrCollection, toggleListOrCollection] = useState('list')
+export default function CollectionsListItem({ collection }) {
+  const [modalVisible, setModalVisibilty] = useState(false);
+  const [recordings, setRecordings] = useState([]);
 
+  useEffect(() => {
+    //get recordings from collection
+    const fetchContent = async () => {
+      await axios.get(`https://aloud-server.appspot.com/collection/${collection.id}`)
+        .then(response => {
+          setRecordings(response.data);
+        })
+        .catch(err => console.log('there was an axios err', err))
+    };
 
-if(listOrCollection === 'list'){
-  return(
+    fetchContent();
+  }, [collection]);
 
-  <TouchableWithoutFeedback onPress={()=> {toggleListOrCollection('collection')}}>
+  const onPressHandle = () => {
+    console.log('COLLECTION', collection);
+    setModalVisibilty(!modalVisible);
+  };
 
-    <Card
-    containerStyle={{ padding: 0, width: 150, height: 150, borderWidth: 0, marginBottom: 15 }}
-    image={{ uri: collection.url_image } }
-    // title={collection.title} 
-    featuredSubtitle={collection.title}
-    />
-  </TouchableWithoutFeedback>
-  )
-} else {
+  if (modalVisible) {
+    return (
+      <Modal
+        animationType="fade"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisibilty(!modalVisible)
+        }}>
+        <Button
+          icon={{
+            name: "chevron-left",
+            size: 25,
+            color: "red"
+          }}
+          type="clear"
+          onPress={() => {
+            setModalVisibilty(!modalVisible);
+          }}
+        />
+        <Image
+          source={{ uri: collection.url_image }}
+          containerStyle={{ marginTop: 30, marginLeft: 15, width: 100, height: 100 }}
+        />
+        <Text style={{ marginTop: 15, marginLeft: 15, textAlign: "left" }}>{collection.title}</Text>
+        <Text style={{ marginLeft: 15, textAlign: "left" }}>{collection.username}</Text>
+        <Text style={{ margin: 15, marginBottom: 15, textAlign: "left" }}>{collection.description}</Text>
+        <Text style={{ marginLeft: 15, marginBottom: 15, textAlign: "left" }}>add options like save collection</Text>
+        <RecordingsList recordings={recordings} />
+      </Modal>
+    ) 
+  }
+
   return (
-    <CollectionsScreen collection={collection}/>
- 
+    <TouchableWithoutFeedback onPress={() => { onPressHandle() }}>
+      <Card
+        containerStyle={{ padding: 0, width: 150, height: 150, borderWidth: 0, marginBottom: 15 }}
+        image={{ uri: collection.url_image }}
+        // title={collection.title} 
+        featuredSubtitle={collection.title}
+      />
+    </TouchableWithoutFeedback>
   )
-}
-  
 
-   
 }

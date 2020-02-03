4323907e504d7c9e89b73884e75f6cc1135486a9
addupe <autumndupin@gmail.com>
2020-02-03 10:13:34 -0600

    add working search bar (sounds only)

--- aloud/screens/SearchScreen.js
+++ aloud/screens/SearchScreen.js
@@ -2,42 +2,48 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ExpoConfigView } from '@expo/samples';
 import CollectionsList from '../components/Lists/CollectionsList';
-import RecordingsList from '../components/Lists/RecordingsList';
+import RecordingsListItem from '../components/ListItems/RecordingsListItem';
 import {Image,Platform,ScrollView,StyleSheet,Text,TextInput,TouchableOpacity,View,
 } from 'react-native';
+import {SearchBar} from 'react-native-elements';
 import SearchStack from '../navigation/SearchNavigator';
-import ButtonGroup from 'react-native-elements';
 
 export default function SearchScreen() {
-  const [value, onChangeText] = React.useState('search here');
-  const [collections, setCollections] = React.useState([]);
+  const [searchTerm, onChangeText] = React.useState('search...');
+  // const [collections, setCollections] = React.useState([]);
   const [recordings, setRecordings] = React.useState([]);
 
-  useEffect(() => {
+  const handleSubmit = (() => {
+    console.log('hit submit', searchTerm);
     const fetchContent = async () => {
-      await axios.get('https://aloud-server.appspot.com/home/1')
+      await axios.post(`http://aloud-server.appspot.com/query/${searchTerm}`)
         .then(response => {
-          setCollections(response.data[0].collections);
-          setRecordings(response.data[0].recordings);
+          console.log(response.data);
+          setRecordings(response.data);
         })
         .catch(err => console.log('there was an axios err', err))
     };
 
     fetchContent();
-  }, []);
+  });
 
   return (
   <ScrollView>
-    {/* <Text>search bar</Text> */}
-    {/* <SearchStack/> */}
-      <TextInput
-        style={{ height: 40, borderColor: 'black', borderWidth: 0.5, margin: 10 }}
+      <SearchBar
+        lightTheme
+        round
+        platform="android"
+        searchIcon={{ size: 20, color: 'red' }}
+        clearIcon={{ size: 20, color: '#eac2cd' }}
+        inputStyle={{ fontStyle: "italic", fontSize: 16}}
+        onSubmitEditing={handleSubmit}
         onChangeText={text => onChangeText(text)}
-        value={value}
+        value={searchTerm}
       />
       <View>
-        <CollectionsList collections={collections} />
-        <RecordingsList recordings={recordings} />
+      {recordings.map(recording => {
+        return <RecordingsListItem recording={recording.item} />
+      })}
       </View>
   </ScrollView>
   );

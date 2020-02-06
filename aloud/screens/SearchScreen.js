60f99852fb5a53aff993a0f4f4edddd6ecb3833c
addupe <autumndupin@gmail.com>
2020-02-06 11:17:44 -0600

    add no results option to search

--- aloud/screens/SearchScreen.js
+++ aloud/screens/SearchScreen.js
@@ -10,9 +10,10 @@ import {SearchBar} from 'react-native-elements';
 import SearchStack from '../navigation/SearchNavigator';
 
 export default function SearchScreen() {
-  const [searchTerm, onChangeText] = React.useState('search...');
+  const [searchTerm, onChangeText] = React.useState('');
   // const [collections, setCollections] = React.useState([]);
   const [recordings, setRecordings] = React.useState([]);
+  const [noMatch, setNoMatch] = React.useState(false);
   const [defaultRecordings, setDefaultRecordings] = React.useState([]);
   const [collections, setDefaultCollections] = React.useState([]);
 
@@ -27,6 +28,7 @@ export default function SearchScreen() {
     };
 
     fetchContent();
+    setRecordings([]);
   }, []);
 
   const handleSubmit = (() => {
@@ -34,13 +36,43 @@ export default function SearchScreen() {
     const fetchContent = async () => {
       await axios.post(`http://aloud-server.appspot.com/query/${searchTerm}`)
         .then(response => {
+          if (!response.data.length) {
+            setNoMatch(true);
+          } else {
           setRecordings(response.data);
+          setNoMatch(false);
+          }
         })
         .catch(err => console.log('there was an axios err', err))
     };
 
     fetchContent();
   });
+
+  if (noMatch) {
+    return (
+      <ScrollView>
+        <SearchBar
+          lightTheme
+          round
+          platform="android"
+          searchIcon={{ size: 20, color: 'red' }}
+          clearIcon={{ size: 20, color: '#eac2cd' }}
+          inputStyle={{ fontStyle: "italic", fontSize: 16 }}
+          onSubmitEditing={handleSubmit}
+          onChangeText={text => {
+            onChangeText(text)
+            setNoMatch(false);
+          }}
+          value={searchTerm}
+        />
+        <View>
+          <Text style={{marginLeft: 22, marginBottom: 10, color: 'red'}}>no results, try again!</Text> 
+          <RecordingsList recordings={defaultRecordings} />
+        </View>
+      </ScrollView>
+    )
+  };
 
   if (!recordings.length) {
     return (
@@ -53,7 +85,10 @@ export default function SearchScreen() {
         clearIcon={{ size: 20, color: '#eac2cd' }}
         inputStyle={{ fontStyle: "italic", fontSize: 16 }}
         onSubmitEditing={handleSubmit}
-        onChangeText={text => onChangeText(text)}
+        onChangeText={text => {
+            onChangeText(text)
+            setNoMatch(false);
+        }}
         value={searchTerm}
       />
       <View>
